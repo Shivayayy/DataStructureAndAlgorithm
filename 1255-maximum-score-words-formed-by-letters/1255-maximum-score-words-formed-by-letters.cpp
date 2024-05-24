fa07a9f45b4903dd4d3e2346c51db544f6f5f9ef
Shivayayy <119866528+Shivayayy@users.noreply.github.com>
2024-05-24 17:11:17 +0530

    Time: 10 ms (57.67%), Space: 16.9 MB (73.59%) - LeetHub

---
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void scorer(vector<string>& words, vector<int>& count, vector<int>& score,int ind,int sum,int &ans)
+    {
+        if(ind == words.size())
+        {
+            ans =max(sum,ans);
+            return;
+        }
+        scorer(words,count,score,ind+1,sum,ans);
+        int temp =0;
+        vector<int>tempCount =count;
+        bool flag =false;
+        for(int i=0;i<words[ind].size();i++)
+        {
+            int alp =words[ind][i]-'a';
+            if(count[alp]>0)
+            {
+                count[alp]--;
+                temp += score[alp];
+            }
+            else
+            {
+                flag =true;
+                break;
+            }
+        }
+        if(!flag)scorer(words,count,score,ind+1,sum+temp,ans);
+        count = tempCount;
+
+    }
+    int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) 
+    {
+        vector<int>count(26,0);
+        for(int i=0;i<letters.size();i++)count[letters[i]-'a']++;
+        int ans =INT_MIN;
+        scorer(words,count,score,0,0,ans);
+        return ans;
+    }
+};
